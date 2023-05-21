6449dc1f0b6096c2c50dd05696d6293c967c65b8
chittaranjan <chittaranjan.gumma@motorolasolutions.com>
2023-05-21 21:34:40 +0530

    notMajor 1

--- src/CalC.tsx
+++ src/CalC.tsx
@@ -3,27 +3,30 @@ import cal from './Operations'
 import React, { useRef, useState } from 'react';
 
 
-let lst = [];
-for(let i=0;i<=9;i++){
-    lst.push(i);
-}
-const buttons = lst.map(i => <button>{i}</button>);
-function addSymbol(){
-
-}
 function CalC(){
     const inputRef = useRef<HTMLInputElement>(null);
     const symRef = useRef<HTMLButtonElement>(null);
     const [displayText, setDisplayText] = useState('');
+
     const addSymbolsToInput = (event:any) => {
         let inputValue = inputRef.current?.value;
+        // let inputValue = document.getElementById('query') instanceof HTMLInputElement?
         let symbol = event.target.id;
         setDisplayText(inputValue+''+symbol);
         console.log(inputValue+''+symbol)
     };
+
     const handleInputChange = (event: any) => {
         setDisplayText(event.target.value);
     }
+
+    let lst = [];
+    for(let i=0;i<=9;i++){
+        lst.push(i);
+    }
+    const buttons = lst.map(i => <button id={i.toString()} onClick={addSymbolsToInput} >{i}</button>);
+
+
     return (
         <>
         <h1 style={{color:"white"}}>Welcome to calc for pro's</h1>
